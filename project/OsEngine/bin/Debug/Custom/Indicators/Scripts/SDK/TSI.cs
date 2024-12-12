2bcbfa8ccc3a9280b08726b0d3605651db6ef731
Sergey Galkin <gsergeysdk@gmail.com>
2024-12-12 21:21:40 +0300

    добавлены индикаторы

---
+++ project/OsEngine/bin/Debug/Custom/Indicators/Scripts/SDK/TSI.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using OsEngine.Entity;
+using OsEngine.Indicators;
+
+namespace CustomIndicators.Scripts
+{
+    public class TSI : Aindicator
+    {
+        private IndicatorParameterInt _fastlength;
+        private IndicatorParameterInt _slowlength;
+        private IndicatorDataSeries _series;
+        private IndicatorDataSeries _seriesEma1;
+        private IndicatorDataSeries _seriesEma2;
+        private IndicatorDataSeries _seriesEma3;
+        private IndicatorDataSeries _seriesEma4;
+
+        public override void OnStateChange(IndicatorState state)
+        {
+            if (state == IndicatorState.Configure)
+            {
+                _fastlength = CreateParameterInt("Period fast", 20);
+                _slowlength = CreateParameterInt("Period slow", 100);
+
+                _series = CreateSeries("Values", Color.OrangeRed, IndicatorChartPaintType.Column, true);
+                _seriesEma1 = CreateSeries("Ema1", Color.DarkRed, IndicatorChartPaintType.Line, false);
+                _seriesEma2 = CreateSeries("Ema2", Color.DarkRed, IndicatorChartPaintType.Line, false);
+                _seriesEma3 = CreateSeries("Ema3", Color.DarkRed, IndicatorChartPaintType.Line, false);
+                _seriesEma4 = CreateSeries("Ema4", Color.DarkRed, IndicatorChartPaintType.Line, false);
+            }
+        }
+
+        public override void OnProcess(List<Candle> candles, int index)
+        {
+            //((Fast MA – Slow MA) / |Slow MA|) * 100
+            //_series.Values[index] = ((_fastsma.DataSeries[0].Values[index] - _slowsma.DataSeries[0].Values[index]) / Math.Abs(_slowsma.DataSeries[0].Values[index])) * 100.0m;
+
+            // 100*EMA_1(EMA_2(m, fl), sl)/EMA_3(EMA_4(|m|, fl), sl)
+            decimal momentum = index > 0 ? candles[index].Close - candles[index - 1].Close : 0m;
+            ProcessEma(_seriesEma2, index, momentum, _fastlength);
+            ProcessEma(_seriesEma4, index, Math.Abs(momentum), _fastlength);
+            ProcessEma(_seriesEma1, index, _seriesEma2.Values[index], _slowlength);
+            ProcessEma(_seriesEma3, index, _seriesEma4.Values[index], _slowlength);
+            if (_seriesEma3.Values[index] != 0m)
+                _series.Values[index] = 100.0m * _seriesEma1.Values[index] / _seriesEma3.Values[index];
+        }
+
+        private void ProcessEma(IndicatorDataSeries _series, int index, decimal val, IndicatorParameterInt length)
+        {
+            decimal result = 0;
+
+            if (index == 0)
+            {
+                result = val;
+            }
+            else
+            {
+                decimal a = Math.Round(2.0m / (length.ValueInt + 1), 8);
+                decimal emaLast = _series.Values[index - 1];
+                result = emaLast + (a * (val - emaLast));
+            }
+
+            _series.Values[index] = Math.Round(result, 8);
+        }
+
+    }
+}
---
+++ project/OsEngine/bin/Debug/Custom/Indicators/Scripts/SDK/TrandPhase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using OsEngine.Entity;
+using OsEngine.Indicators;
+
+namespace CustomIndicators.Scripts
+{
+    public class TrandPhase : Aindicator
+    {
+        private IndicatorParameterInt _length_s;
+        private IndicatorParameterInt _length_b;
+        private IndicatorParameterInt _trand_counter;
+        private IndicatorDataSeries _seriesL;
+        private IndicatorDataSeries _seriesS;
+        private IndicatorDataSeries _seriesLongCount;
+        private IndicatorDataSeries _seriesShortCount;
+        private Aindicator _ma_s;
+        private Aindicator _ma_b;
+
+        public override void OnStateChange(IndicatorState state)
+        {
+            if (state == IndicatorState.Configure)
+            {
+                _length_s = CreateParameterInt("Period fast", 200);
+                _length_b = CreateParameterInt("Period slow", 500);
+                _trand_counter = CreateParameterInt("Trand counter", 10);
+
+                _seriesL = CreateSeries("ValuesLong", Color.Green, IndicatorChartPaintType.Column, true);
+                _seriesS = CreateSeries("ValuesShort", Color.Red, IndicatorChartPaintType.Column, true);
+                _seriesLongCount = CreateSeries("Longs", Color.DarkGreen, IndicatorChartPaintType.Line, false);
+                _seriesShortCount = CreateSeries("Shorts", Color.DarkRed, IndicatorChartPaintType.Line, false);
+
+                _ma_s = IndicatorsFactory.CreateIndicatorByName("Ssma", Name + "Ssma", false);
+                ((IndicatorParameterInt)_ma_s.Parameters[0]).Bind(_length_s);
+                ProcessIndicator("MA_S", _ma_s);
+                _ma_b = IndicatorsFactory.CreateIndicatorByName("Ssma", Name + "Ssma", false);
+                ((IndicatorParameterInt)_ma_b.Parameters[0]).Bind(_length_b);
+                ProcessIndicator("MA_B", _ma_b);
+            }
+        }
+
+        public override void OnProcess(List<Candle> candles, int index)
+        {
+            if (index < _length_s.ValueInt || index < _length_b.ValueInt)
+            {
+                _seriesL.Values[index] = 0m;
+                _seriesS.Values[index] = 0m;
+                _seriesLongCount.Values[index] = 0m;
+                _seriesShortCount.Values[index] = 0m;
+                return;
+            }
+
+            decimal currVal = candles[index].Close;
+            bool upS = currVal > _ma_s.DataSeries[0].Values[index];
+            bool upB = currVal > _ma_b.DataSeries[0].Values[index];
+            bool downS = currVal < _ma_s.DataSeries[0].Values[index];
+            bool downB = currVal < _ma_b.DataSeries[0].Values[index];
+
+            if (upS && upB)
+            {
+                _seriesShortCount.Values[index] = 0m;
+                _seriesLongCount.Values[index] = _seriesLongCount.Values[index-1] + 1m;
+            }
+            else if (downS && downB)
+            {
+                _seriesLongCount.Values[index] = 0m;
+                _seriesShortCount.Values[index] = _seriesShortCount.Values[index-1] + 1m;
+            }
+            else
+            {
+                _seriesLongCount.Values[index] = 0m;
+                _seriesShortCount.Values[index] = 0m;
+            }
+
+            _seriesL.Values[index] = _seriesLongCount.Values[index] > _trand_counter.ValueInt ? 1.0m : 0m;
+            _seriesS.Values[index] = _seriesShortCount.Values[index] > _trand_counter.ValueInt ? 1.0m : 0m;
+
+        }
+    }
+}
