d3cb13d45a66f0d472e895d12dc456bca1efc042
Sergey Galkin <gsergeysdk@gmail.com>
2024-12-12 21:21:40 +0300

    добавлены индикаторы

---
+++ project/OsEngine/bin/Debug/Custom/Indicators/Scripts/SDK/DEMA_ssma.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using OsEngine.Entity;
+using OsEngine.Indicators;
+
+namespace CustomIndicators.Scripts
+{
+    public class DEMA_ssma : Aindicator
+    {
+        private IndicatorParameterInt length;
+        private IndicatorDataSeries _series;
+        private IndicatorDataSeries _seriesEma1;
+        private IndicatorDataSeries _seriesEma2;
+
+        public override void OnStateChange(IndicatorState state)
+        {
+            if (state == IndicatorState.Configure)
+            {
+                length = CreateParameterInt("Period", 20);
+
+                _series = CreateSeries("Values", Color.Red, IndicatorChartPaintType.Line, true);
+                _seriesEma1 = CreateSeries("Ema1", Color.DarkRed, IndicatorChartPaintType.Line, false);
+                _seriesEma2 = CreateSeries("Ema2", Color.DarkRed, IndicatorChartPaintType.Line, false);
+            }
+        }
+
+        public override void OnProcess(List<Candle> candles, int index)
+        {
+            // DEMA(i) = 2*EMA_1(price, length) - EMA_2(EMA_1(price, length), length)
+            decimal price = candles[index].Close;
+            ProcessEma(_seriesEma1, index, price, length);
+            ProcessEma(_seriesEma2, index, _seriesEma1.Values[index], length);
+            _series.Values[index] = 2.0m * _seriesEma1.Values[index] - _seriesEma2.Values[index];
+        }
+
+        private void ProcessEma(IndicatorDataSeries _series, int index, decimal val, IndicatorParameterInt length)
+        {
+            decimal result = 0;
+
+            if (index == 0)
+            {
+                result = val;
+            }
+            else
+            {
+                //decimal a = Math.Round(2.0m / (length.ValueInt + 1), 8);
+                //decimal emaLast = _series.Values[index - 1];
+                //result = emaLast + (a * (val - emaLast));
+                decimal ssmaLast = _series.Values[index - 1];
+                result = (ssmaLast * (length.ValueInt - 1) + val) / length.ValueInt;
+            }
+
+            _series.Values[index] = Math.Round(result, 8);
+        }
+
+    }
+}
