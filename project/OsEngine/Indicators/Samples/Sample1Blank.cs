4eea69b1d7617ffcb3791382b9b5a111283255b1
unknown <alexey@o-s-a.net>
2024-12-19 08:43:39 +0000

    New indicator sample: Sample1Blank

--- project/OsEngine/Indicators/Samples/Sample1Blank.cs
+++ project/OsEngine/Indicators/Samples/Sample1Blank.cs
@@ -6,13 +6,14 @@ namespace OsEngine.Indicators.Samples
     [Indicator("Sample1Blank")]
     public class Sample1Blank : Aindicator
     {
+        public IndicatorDataSeries Series;
 
         public override void OnStateChange(IndicatorState state)
         {
             if(state == IndicatorState.Configure)
             { // Instead of a constructor
 
-            
+                Series = CreateSeries("Series 1", System.Drawing.Color.Blue, IndicatorChartPaintType.Line, true);
 
             }
         }
@@ -20,6 +21,7 @@ namespace OsEngine.Indicators.Samples
         public override void OnProcess(List<Candle> source, int index)
         {
 
+            Series.Values[index] = source[index].Center;
 
         }
     }
