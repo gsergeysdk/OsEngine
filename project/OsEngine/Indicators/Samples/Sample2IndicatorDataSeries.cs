3c7d8e6b9bdd75780676ac8113fc955de31684bd
unknown <alexey@o-s-a.net>
2024-12-19 08:05:16 +0000

    Indicators layer refactoring

--- project/OsEngine/Indicators/Samples/Sample2IndicatorDataSeries.cs
+++ project/OsEngine/Indicators/Samples/Sample2IndicatorDataSeries.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OsEngine.Indicators
+namespace OsEngine.Indicators.Samples
 {
-    public class IndicatorDataSeries
+    internal class Sample2IndicatorDataSeries
     {
     }
 }
