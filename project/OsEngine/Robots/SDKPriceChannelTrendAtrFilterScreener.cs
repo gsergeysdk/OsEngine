9e06a860dae1a4fecf987366a488fb129a4f0502
Sergey Galkin <gsergeysdk@gmail.com>
2025-01-06 23:03:58 +0300

    Учет заблокированнных денег в портфолио

--- project/OsEngine/Robots/SDKPriceChannelTrendAtrFilterScreener.cs
+++ project/OsEngine/Robots/SDKPriceChannelTrendAtrFilterScreener.cs
@@ -258,10 +258,12 @@ namespace OsEngine.Robots.SDKRobots
                 }
 
                 decimal portfolioPrimeAsset = 0;
+                decimal portfolioPrimeAssetBlocked = 0;
 
                 if (TradeAssetInPortfolio.ValueString == "Prime")
                 {
                     portfolioPrimeAsset = myPortfolio.ValueCurrent;
+                    portfolioPrimeAssetBlocked = myPortfolio.ValueBlocked;
                 }
                 else
                 {
@@ -277,6 +279,7 @@ namespace OsEngine.Robots.SDKRobots
                         if (positionOnBoard[i].SecurityNameCode == TradeAssetInPortfolio.ValueString)
                         {
                             portfolioPrimeAsset = positionOnBoard[i].ValueCurrent;
+                            portfolioPrimeAssetBlocked = positionOnBoard[i].ValueBlocked;
                             break;
                         }
                     }
@@ -288,7 +291,7 @@ namespace OsEngine.Robots.SDKRobots
                     return 0;
                 }
 
-                decimal moneyOnPosition = portfolioPrimeAsset * (Volume.ValueDecimal / 100);
+                decimal moneyOnPosition = Math.Min(portfolioPrimeAsset * (Volume.ValueDecimal / 100), portfolioPrimeAsset - portfolioPrimeAssetBlocked);
 
                 decimal qty = moneyOnPosition / tab.PriceBestAsk / tab.Security.Lot;
 
