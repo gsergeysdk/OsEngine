475a054a51aa093f702ef867c49ebdab3e8a4903
Sergey Galkin <gsergeysdk@gmail.com>
2025-02-23 20:29:06 +0300

    Отдельные модули для Объемов, и фильтров

--- project/OsEngine/Robots/SDKThreeSoldierScreener.cs
+++ project/OsEngine/Robots/SDKThreeSoldierScreener.cs
@@ -8,6 +8,8 @@ using OsEngine.OsTrader.Panels.Attributes;
 using OsEngine.OsTrader.Panels.Tab;
 using System.IO;
 using System.Globalization;
+using OsEngine.Charts.CandleChart.Indicators;
+
 
 namespace OsEngine.Robots.SoldiersScreener
 {
@@ -25,10 +27,6 @@ namespace OsEngine.Robots.SoldiersScreener
            
             Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort" });
             MaxPositions = CreateParameter("Max positions", 5, 0, 20, 1);
-            VolumeType = CreateParameter("Volume type", "Contracts", new[] { "Contracts", "Contract currency", "Deposit percent" });
-            Volume = CreateParameter("Volume", 1, 1.0m, 50, 4);
-            FullTradeAssetInPortfolio = CreateParameter("Full Asset in portfolio", "Prime");
-            TradeAssetInPortfolio = CreateParameter("Asset in portfolio (limit)", "Prime");
             Slippage = CreateParameter("Slippage %", 0, 0, 20, 1m);
             ProcHeightTake = CreateParameter("Profit % from height of pattern", 50m, 0, 20, 1m);
             ProcHeightStop = CreateParameter("Stop % from height of pattern", 20m, 0, 20, 1m);
@@ -39,6 +37,7 @@ namespace OsEngine.Robots.SoldiersScreener
             MinHeightOneSoldiersVolaPecrent = CreateParameter("Min height one soldier volatility percent", 1, 0, 20, 1m);
             SmaFilterIsOn = CreateParameter("Sma filter is on", true);
             SmaFilterLen = CreateParameter("Sma filter Len", 100, 100, 300, 10);
+            volume = new SDKVolume(this);
 
             Description = "Trading robot Three Soldiers adaptive by volatility. " +
                 "When forming a pattern of three growing / falling candles, " +
@@ -71,10 +70,6 @@ namespace OsEngine.Robots.SoldiersScreener
         public StrategyParameterDecimal TrailingStopRepcent;
         public StrategyParameterInt ExitAtBarCount;
         public StrategyParameterDecimal Slippage;
-        public StrategyParameterString VolumeType;
-        public StrategyParameterDecimal Volume;
-        public StrategyParameterString TradeAssetInPortfolio;
-        public StrategyParameterString FullTradeAssetInPortfolio;
 
         public StrategyParameterInt DaysVolatilityAdaptive;
         public StrategyParameterDecimal HeightSoldiersVolaPecrent;
@@ -82,6 +77,8 @@ namespace OsEngine.Robots.SoldiersScreener
 
         public StrategyParameterBool SmaFilterIsOn;
         public StrategyParameterInt SmaFilterLen;
+
+        public SDKVolume volume;
 
         // volatility adaptation
 
@@ -344,9 +341,9 @@ namespace OsEngine.Robots.SoldiersScreener
                         if (smaValue < smaPrev)
                             return;
                     }
-                    decimal volume = GetVolume(tab);
-                    if (volume > 0)
-                        tab.BuyAtLimit(volume, _lastPrice + _lastPrice * (Slippage.ValueDecimal / 100));
+                    decimal vol = volume.GetVolume(tab);
+                    if (vol > 0)
+                        tab.BuyAtLimit(vol, _lastPrice + _lastPrice * (Slippage.ValueDecimal / 100));
                 }
             }
 
@@ -364,9 +361,9 @@ namespace OsEngine.Robots.SoldiersScreener
                         if (smaValue > smaPrev)
                             return;
                     }
-                    decimal volume = GetVolume(tab);
-                    if (volume > 0)
-                        tab.SellAtLimit(volume, _lastPrice - _lastPrice * (Slippage.ValueDecimal / 100));
+                    decimal vol = volume.GetVolume(tab);
+                    if (vol > 0)
+                        tab.SellAtLimit(vol, _lastPrice - _lastPrice * (Slippage.ValueDecimal / 100));
                 }
             }
             return;
@@ -445,96 +442,6 @@ namespace OsEngine.Robots.SoldiersScreener
                     }
                 }
             }
-        }
-
-        private decimal GetVolume(BotTabSimple tab)
-        {
-            decimal volume = 0;
-
-            if (VolumeType.ValueString == "Contracts")
-            {
-                volume = Volume.ValueDecimal;
-            }
-            else if (VolumeType.ValueString == "Contract currency")
-            {
-                decimal contractPrice = tab.PriceBestAsk;
-                volume = Volume.ValueDecimal / contractPrice;
-
-                if (StartProgram == StartProgram.IsOsTrader)
-                {
-                    IServerPermission serverPermission = ServerMaster.GetServerPermission(tab.Connector.ServerType);
-
-                    if (serverPermission != null &&
-                        serverPermission.IsUseLotToCalculateProfit &&
-                    tab.Security.Lot != 0 &&
-                        tab.Security.Lot > 1)
-                    {
-                        volume = Volume.ValueDecimal / (contractPrice * tab.Security.Lot);
-                    }
-
-                    volume = Math.Round(volume, tab.Security.DecimalsVolume);
-                }
-                else // Tester or Optimizer
-                {
-                    volume = Math.Round(volume / tab.Security.Lot, 6);
-                }
-            }
-            else if (VolumeType.ValueString == "Deposit percent")
-            {
-                Portfolio myPortfolio = tab.Portfolio;
-
-                if (myPortfolio == null)
-                {
-                    return 0;
-                }
-
-                decimal portfolioPrimeAsset = 0;
-                decimal fullPortfolioPrimeAsset = 0;
-
-                if (FullTradeAssetInPortfolio.ValueString == "Prime")
-                    fullPortfolioPrimeAsset = myPortfolio.ValueCurrent;
-                if (TradeAssetInPortfolio.ValueString == "Prime")
-                    portfolioPrimeAsset = myPortfolio.ValueCurrent;
-                {
-                    List<PositionOnBoard> positionOnBoard = myPortfolio.GetPositionOnBoard();
-
-                    if (positionOnBoard == null)
-                    {
-                        return 0;
-                    }
-
-                    for (int i = 0; i < positionOnBoard.Count; i++)
-                    {
-                        if (positionOnBoard[i].SecurityNameCode == TradeAssetInPortfolio.ValueString)
-                            portfolioPrimeAsset = positionOnBoard[i].ValueCurrent;
-                        if (positionOnBoard[i].SecurityNameCode == FullTradeAssetInPortfolio.ValueString)
-                            fullPortfolioPrimeAsset = positionOnBoard[i].ValueCurrent;
-                    }
-                }
-
-                if (portfolioPrimeAsset == 0 || fullPortfolioPrimeAsset == 0)
-                {
-                    SendNewLogMessage("Can`t found portfolio " + TradeAssetInPortfolio.ValueString, Logging.LogMessageType.Error);
-                    return 0;
-                }
-
-                decimal moneyOnPosition = Math.Min(fullPortfolioPrimeAsset * (Volume.ValueDecimal / 100), portfolioPrimeAsset);
-
-                decimal qty = moneyOnPosition / tab.PriceBestAsk / tab.Security.Lot;
-
-                if (tab.StartProgram == StartProgram.IsOsTrader)
-                {
-                    qty = Math.Round(qty, tab.Security.DecimalsVolume, MidpointRounding.ToEven);
-                }
-                else
-                {
-                    qty = Math.Round(qty, 7);
-                }
-
-                return qty;
-            }
-
-            return volume;
         }
 
         private decimal Sma(List<Candle> candles, int len, int index)
